f6194a4b952d9740ee42a4ae04d59b5ef5f9d75d
Chris Hua <hua.christopher@gmail.com>
2023-08-12 13:41:08 -0400

    feat: use kong

--- main.go
+++ main.go
@@ -6,74 +6,85 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"github.com/alecthomas/kong"
 	"github.com/fsnotify/fsnotify"
 )
 
+var CLI struct {
+	Watch struct {
+		GlobPattern string   `arg:"" required:"" name:"glob" help:"Glob pattern to watch files." type:"string"`
+		Command     string   `arg:"" required:"" name:"command" help:"Command to run upon file changes." type:"string"`
+		Args        []string `arg:"" required:"" name:"args" help:"Additional arguments for the command."`
+	} `cmd:"" help:"Watch files and run a command upon changes."`
+}
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: watch <glob_pattern> <command> [args...]")
-		return
-	}
-
-	globPattern := os.Args[1]
-	cmdArgs := os.Args[2:]
-
-	// Set up fsnotify watcher
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		fmt.Printf("Error creating watcher: %v\n", err)
-		return
-	}
-	defer watcher.Close()
-
-	// Initially find and watch all matching files
-	files, err := filepath.Glob(globPattern)
-	if err != nil {
-		fmt.Printf("Error matching files: %v\n", err)
-		return
-	}
-
-	for _, file := range files {
-		err = watcher.Add(file)
+	ctx := kong.Parse(&CLI)
+	// fmt.Printf("Command: %s\n", ctx.Command())
+	switch ctx.Command() {
+	case "watch <glob> <command> <args>":
+		globPattern := CLI.Watch.GlobPattern
+		cmdArgs := append([]string{CLI.Watch.Command}, CLI.Watch.Args...)
+		watcher, err := fsnotify.NewWatcher()
 		if err != nil {
-			fmt.Printf("Error watching file %s: %v\n", file, err)
-			continue
+			fmt.Printf("Error creating watcher: %v\n", err)
+			return
 		}
-	}
-	fmt.Printf("Watching %d files matching %s\n", len(files), globPattern)
+		defer watcher.Close()
 
-	// Handle events
-	for {
-		select {
-		case event, ok := <-watcher.Events:
-			if !ok {
-				return
-			}
-
-			if event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
-				fmt.Printf("File %s changed, running command: %v\n", event.Name, cmdArgs)
-				cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				err := cmd.Run()
-				if err != nil {
-					fmt.Printf("Error running command: %v\n", err)
-				}
-			}
-
-			// If new files are added matching the pattern, start watching them
-			newFiles, err := filepath.Glob(globPattern)
-			for _, file := range newFiles {
-				if err = watcher.Add(file); err != nil {
-					fmt.Printf("Error watching new file %s: %v\n", file, err)
-				}
-			}
-
-		case err, ok := <-watcher.Errors:
-			if !ok {
-				return
-			}
-			fmt.Printf("Watcher error: %v\n", err)
+		files, err := filepath.Glob(globPattern)
+		if err != nil {
+			fmt.Printf("Error matching files: %v\n", err)
+			return
 		}
+
+		if len(files) == 0 {
+			fmt.Printf("No files found matching %s\n", globPattern)
+			return
+		}
+
+		for _, file := range files {
+			err = watcher.Add(file)
+			if err != nil {
+				fmt.Printf("Error watching file %s: %v\n", file, err)
+				continue
+			}
+		}
+		fmt.Printf("Watching %d files matching %s\n", len(files), globPattern)
+
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+
+				if event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
+					cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+					cmd.Stdout = os.Stdout
+					cmd.Stderr = os.Stderr
+					err := cmd.Run()
+					if err != nil {
+						fmt.Printf("Error running command: %v\n", err)
+					}
+				}
+
+				newFiles, err := filepath.Glob(globPattern)
+				for _, file := range newFiles {
+					if err = watcher.Add(file); err != nil {
+						fmt.Printf("Error watching new file %s: %v\n", file, err)
+					}
+				}
+
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				fmt.Printf("Watcher error: %v\n", err)
+			}
+		}
+
+	default:
+		panic(ctx.Command())
 	}
 }
